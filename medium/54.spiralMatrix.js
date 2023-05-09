5169160a91df6cd93e98449587afff2c5ed1bb1e
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-05-09 11:42:09 +0400

    feat(medium): add 54.spiralMatrix task

---
+++ medium/54.spiralMatrix.js
@@ -0,0 +1,48 @@
+/*
+  Given an m x n matrix, return all elements of the matrix in spiral order.
+
+  Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+*/
+
+var spiralOrder = function (matrix) {
+  const result = [];
+
+  let verCoord = Math.ceil((matrix.length - 1) / 2);
+  let horCoord = Math.ceil((matrix[0].length - 1) / 2);
+
+  let position = "left";
+
+  while (verCoord >= 0 && horCoord >= 0) {
+    result.unshift(matrix[verCoord][horCoord]);
+
+    if (horCoord === 0) {
+      position = "bottom";
+    }
+    if (verCoord === matrix.length - 1) {
+      position = "right";
+    }
+    if (horCoord === matrix[0].length - 1) {
+      position = "top";
+    }
+    if (verCoord === 0) {
+      position = "left";
+    }
+
+    switch (position) {
+      case "right":
+        horCoord++;
+        break;
+      case "bottom":
+        verCoord++;
+        break;
+      case "left":
+        horCoord--;
+        break;
+      case "top":
+        verCoord--;
+    }
+  }
+
+  return result;
+};
---
+++ medium/tasks.md
@@ -0,0 +1,14 @@
+## LeetCode Medium Tasks
+
+### 54. Spiral Matrix
+
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+![](./assets/spiral.jpg)
+
+| Input                              | Output                       |
+| ---------------------------------- | ---------------------------- |
+| [[1,2,3],[4,5,6],[7,8,9]]          | [1,2,3,6,9,8,7,4,5]          |
+| [[1,2,3,4],[5,6,7,8],[9,10,11,12]] | [1,2,3,4,8,12,11,10,9,5,6,7] |
+
+---
