bfedf4459fdc42148326d6e58eb46e71569a5475
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-05-09 11:59:00 +0400

    feat(54.spiralMatrix): add best solution for me

--- medium/54.spiralMatrix.js
+++ medium/54.spiralMatrix.js
@@ -5,44 +5,67 @@
 Output: [1,2,3,6,9,8,7,4,5]
 */
 
-var spiralOrder = function (matrix) {
-  const result = [];
+// My solution
 
-  let verCoord = Math.ceil((matrix.length - 1) / 2);
-  let horCoord = Math.ceil((matrix[0].length - 1) / 2);
+{
+  const spiralOrder = function (matrix) {
+    const result = [];
 
-  let position = "left";
+    let verCoord = Math.ceil((matrix.length - 1) / 2);
+    let horCoord = Math.ceil((matrix[0].length - 1) / 2);
 
-  while (verCoord >= 0 && horCoord >= 0) {
-    result.unshift(matrix[verCoord][horCoord]);
+    let position = "left";
 
-    if (horCoord === 0) {
-      position = "bottom";
+    while (verCoord >= 0 && horCoord >= 0) {
+      result.unshift(matrix[verCoord][horCoord]);
+
+      if (horCoord === 0) {
+        position = "bottom";
+      }
+      if (verCoord === matrix.length - 1) {
+        position = "right";
+      }
+      if (horCoord === matrix[0].length - 1) {
+        position = "top";
+      }
+      if (verCoord === 0) {
+        position = "left";
+      }
+
+      switch (position) {
+        case "right":
+          horCoord++;
+          break;
+        case "bottom":
+          verCoord++;
+          break;
+        case "left":
+          horCoord--;
+          break;
+        case "top":
+          verCoord--;
+      }
     }
-    if (verCoord === matrix.length - 1) {
-      position = "right";
-    }
-    if (horCoord === matrix[0].length - 1) {
-      position = "top";
-    }
-    if (verCoord === 0) {
-      position = "left";
-    }
 
-    switch (position) {
-      case "right":
-        horCoord++;
-        break;
-      case "bottom":
-        verCoord++;
-        break;
-      case "left":
-        horCoord--;
-        break;
-      case "top":
-        verCoord--;
-    }
-  }
+    return result;
+  };
+}
 
-  return result;
-};
+// Best Solution
+
+{
+  const spiralOrder = function (matrix) {
+    const res = [];
+    while (matrix.length) {
+      const first = matrix.shift();
+      res.push(...first);
+      for (const m of matrix) {
+        let val = m.pop();
+        if (val) res.push(val);
+        m.reverse();
+      }
+      matrix.reverse();
+    }
+    return res;
+  };
+}
