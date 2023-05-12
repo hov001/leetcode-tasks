4e7955bcb382d0c27511b58bb070f5f3dfb84f7e
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-05-12 10:53:09 +0400

    feat(easy): add 2666 task

---
+++ easy/2666.allowOneTimeCall.js
@@ -0,0 +1,28 @@
+/*
+
+Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+The first time the returned function is called, it should return the same result as fn.
+Every subsequent time it is called, it should return undefined.
+
+```
+Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+Output: [{"calls":1,"value":140}]
+Explanation:
+const onceFn = once(fn);
+onceFn(5, 7, 4); // 140
+onceFn(2, 3, 6); // undefined, fn was not called
+onceFn(4, 6, 8); // undefined, fn was not called
+```
+
+*/
+
+var once = function (fn) {
+  let isUsed = false;
+  return function (...args) {
+    if (!isUsed) {
+      isUsed = true;
+      return fn(...args);
+    }
+  };
+};
